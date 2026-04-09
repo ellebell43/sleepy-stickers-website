76ca8db3683c6510bf2f3a2a2013806cdbcdce8e
Elle Brooks <hello@ellebell.dev>
2026-04-08 17:52:15 -0700

    come back later message
    - lightly improved mobile layout for the header
    - padding on the body

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center w-full">
         <Image src="/images/sleepy-stickers-logo.png" alt="Sleepy stickers logo. A pixel art red panda splooted and sleeping on a branch with a green circle as a background with the edges of the subject poking out. The pixel canvas is 64x64" width={64 * 5} height={64 * 5} loading="eager" />
         <h1 className="text-center text-6xl">Sleepy Stickers</h1>
-        <p className="text-center mt-4">Pixel art stickers for every occasion!</p>
+        <p className="text-center mt-4">Wow! You're here early! I'm still working on the site and there isn't anything to see yet. Come back later!</p>
       </div>
     </div>
   );
