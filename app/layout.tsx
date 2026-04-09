4f8b2eb5eeb2d62ebd494aefa278a441d4023a18
Elle Brooks <hello@ellebell.dev>
2026-04-08 17:37:11 -0700

    Header responsiveness
    - header now changes layout depending on size of screen
    - navigation icons now have text next to them on larger screen
    - Header links now have a hover effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 
 const fontAseprite = localFont({ src: '../public/fonts/aseprite.otf/aseprite.otf' })
 
+const iconSize = 16 * 2
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -15,19 +17,23 @@ export default function RootLayout({
       lang="en"
       className={`h-full ${fontAseprite.className}`}
     >
-      <body className={`min-h-full w-full flex flex-col max-w-500 mx-auto antialiased ${fontAseprite.className}`}>
-        <header className="w-full px-5 flex justify-between shadow-md py-2 fixed z-10">
+      <body className={`min-h-full w-full flex flex-col max-w-500 mx-auto antialiased transition-all ${fontAseprite.className}`}>
+        <header className="w-full px-5 flex justify-between md:justify-center md:gap-8 shadow-md pb-2 pt-3 fixed z-10">
           {/* ==========  ABOUT LINK ========== */}
           {/* ========== GALLERY LINK ========== */}
           {/* ========== SHOP LINK ========== */}
-          <Link href="#/shop">
-            <Image src="/images/shop.png" alt="Shop" className="dark:invert" height={16 * 2} width={16 * 2} />
+          <Link href="#/shop" className="hover:opacity-70 transition-all">
+            <div className="flex items-center">
+              <Image src="/images/shop.png" alt="A store front icon on a 16x16 pixel art canvas using just simple black lines" title="Shop" className="dark:invert relative bottom-1" height={iconSize} width={iconSize} />
+              <p className="ml-1 hidden md:block">Shop</p>
+            </div>
           </Link>
           {/* ========== CART LINK ========== */}
-          <Link href="#/cart" className="relative">
+          <Link href="#/cart" className="hover:opacity-70 transition-all">
             <div className="flex items-center">
-              <p className="text-sm relative top-1">0</p>
-              <Image src="/images/cart.png" alt="Shopping Cart" className="dark:invert" height={16 * 2} width={16 * 2} />
+              <p className="text-sm">0</p>
+              <Image src="/images/cart.png" alt="A shopping cart icon on a 16x16 pixel art canvas using just simple black lines" title="Cart" className="dark:invert relative bottom-1" height={iconSize} width={iconSize} />
+              <p className="ml-1 hidden md:block">Cart</p>
             </div>
           </Link>
         </header>
