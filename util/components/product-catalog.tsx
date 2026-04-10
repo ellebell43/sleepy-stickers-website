2dcd35ac1610c479ec089ebf917cb23306ed6528
Elle Brooks <hello@ellebell.dev>
2026-04-10 07:21:10 -0700

    better error reporting

--- util/components/product-catalog.tsx
+++ util/components/product-catalog.tsx
@@ -9,29 +9,24 @@ export default async function ProductCatalog() {
   if (!process.env.HOST_ROUTE) {
     return <p>Oops! Somethings went wrong! Failed to parse host for API route</p>
   }
-  try {
-    const res = await fetch(`${process.env.HOST_ROUTE}/shop/api`)
-    const data = await res.json()
-    return (
-      <Suspense fallback={<Spinner />}>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 w-fit mx-auto mt-8">
-          {data.products.map((el: Stripe.Product, i: number) => {
-            // Ignore any item that is a variant
-            if (el.metadata.variant == "true") return
-            // Create an array of variants that belong to the item
-            let variants: Stripe.Product[] = []
-            data.products.map((item: Stripe.Product, i: number) => {
-              if (item.metadata.variant == "true" && item.metadata.variantOf == el.name) variants.push(item)
-            })
-            // Return a ProductCard element
-            return <ProductCard product={el} key={i} variants={variants} />
-          })}
-        </div>
-      </Suspense>
-    )
-  } catch (error) {
-    return <p>Error! {String(error)}</p>
-  }
-
-
+  const res = await fetch(`${process.env.HOST_ROUTE}/shop/api`)
+  if (res.status != 200) return <p>Error {res.status} :(</p>
+  const data = await res.json()
+  return (
+    <Suspense fallback={<Spinner />}>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 w-fit mx-auto mt-8">
+        {data.products.map((el: Stripe.Product, i: number) => {
+          // Ignore any item that is a variant
+          if (el.metadata.variant == "true") return
+          // Create an array of variants that belong to the item
+          let variants: Stripe.Product[] = []
+          data.products.map((item: Stripe.Product, i: number) => {
+            if (item.metadata.variant == "true" && item.metadata.variantOf == el.name) variants.push(item)
+          })
+          // Return a ProductCard element
+          return <ProductCard product={el} key={i} variants={variants} />
+        })}
+      </div>
+    </Suspense>
+  )
 }
